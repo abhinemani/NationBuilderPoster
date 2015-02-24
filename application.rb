f0af240c7955e653a09367510e5cd2a0ff98f069
Abhi Nemani <abhi.nemani@gmail.com>
2015-02-23 17:36:31 -0800

    added datala info

--- application.rb
+++ application.rb
@@ -6,11 +6,11 @@ class SkeletonNBApp < Sinatra::Base
 	post '/people' do
 	    content_type :json
 	 
-	    nb_client = NationBuilder::Client.new('testnation', ENV['NB_API_KEY'])
+	    nb_client = NationBuilder::Client.new('mayorofla', ENV['63399ad58be8012ed191c1bdeeccac0e257a92efd73494414ebbce0df9848600'])
 	    post_data = {
 	      person: {
 	        email: params[:email],
-	        tags: ["compiler", "compilerla website signup"]
+	        tags: ["datala", "open data"]
 	      }
 	    }
 	 
